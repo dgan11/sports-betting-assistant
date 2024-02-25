f470c8d6ef21815ba8f9282bfe430f71164928b2
David Gan <dgan979@gmail.com>
2024-02-25 08:34:05 -0800

    init with data fetching

---
+++ app/public/placeholder-data.ts
@@ -0,0 +1,341 @@
+export const mockData = [
+  {
+    "id": "05076e0ccc4957d2316861dd24a749f1",
+    "sport_key": "cricket_psl",
+    "sport_title": "Pakistan Super League",
+    "commence_time": "2024-02-25T14:00:00Z",
+    "home_team": "Lahore Qalandars",
+    "away_team": "Peshawar Zalmi",
+    "bookmakers": [
+      {
+        "key": "unibet_us",
+        "title": "Unibet",
+        "last_update": "2024-02-25T16:18:13Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2024-02-25T16:18:13Z",
+            "outcomes": [
+              {
+                "name": "Lahore Qalandars",
+                "price": 3.15
+              },
+              {
+                "name": "Peshawar Zalmi",
+                "price": 1.33
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "key": "bovada",
+        "title": "Bovada",
+        "last_update": "2024-02-25T16:18:14Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2024-02-25T16:18:14Z",
+            "outcomes": [
+              {
+                "name": "Lahore Qalandars",
+                "price": 3.3
+              },
+              {
+                "name": "Peshawar Zalmi",
+                "price": 1.31
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": "d1e9f906a0c852d80a2635ea574469c1",
+    "sport_key": "icehockey_sweden_allsvenskan",
+    "sport_title": "HockeyAllsvenskan",
+    "commence_time": "2024-02-25T14:01:19Z",
+    "home_team": "AIK",
+    "away_team": "Brynäs IF",
+    "bookmakers": [
+      {
+        "key": "unibet_us",
+        "title": "Unibet",
+        "last_update": "2024-02-25T16:12:52Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2024-02-25T16:12:52Z",
+            "outcomes": [
+              {
+                "name": "AIK",
+                "price": 10.5
+              },
+              {
+                "name": "Brynäs IF",
+                "price": 1.02
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "key": "bovada",
+        "title": "Bovada",
+        "last_update": "2024-02-25T16:18:06Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2024-02-25T16:18:06Z",
+            "outcomes": [
+              {
+                "name": "AIK",
+                "price": 16
+              },
+              {
+                "name": "Brynäs IF",
+                "price": 1.02
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": "dfb453040b92f475e39683b6a7f25f13",
+    "sport_key": "soccer_germany_bundesliga",
+    "sport_title": "Bundesliga - Germany",
+    "commence_time": "2024-02-25T14:30:03Z",
+    "home_team": "Eintracht Frankfurt",
+    "away_team": "VfL Wolfsburg",
+    "bookmakers": [
+      {
+        "key": "betmgm",
+        "title": "BetMGM",
+        "last_update": "2024-02-25T16:18:27Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2024-02-25T16:18:27Z",
+            "outcomes": [
+              {
+                "name": "Eintracht Frankfurt",
+                "price": 29
+              },
+              {
+                "name": "VfL Wolfsburg",
+                "price": 1.2
+              },
+              {
+                "name": "Draw",
+                "price": 5
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "key": "fanduel",
+        "title": "FanDuel",
+        "last_update": "2024-02-25T16:17:09Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2024-02-25T16:17:09Z",
+            "outcomes": [
+              {
+                "name": "Eintracht Frankfurt",
+                "price": 30
+              },
+              {
+                "name": "VfL Wolfsburg",
+                "price": 1.22
+              },
+              {
+                "name": "Draw",
+                "price": 4.7
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "key": "bovada",
+        "title": "Bovada",
+        "last_update": "2024-02-25T16:18:25Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2024-02-25T16:18:25Z",
+            "outcomes": [
+              {
+                "name": "Eintracht Frankfurt",
+                "price": 17
+              },
+              {
+                "name": "VfL Wolfsburg",
+                "price": 1.25
+              },
+              {
+                "name": "Draw",
+                "price": 4.3
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "key": "pointsbetus",
+        "title": "PointsBet (US)",
+        "last_update": "2024-02-25T16:18:25Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2024-02-25T16:18:25Z",
+            "outcomes": [
+              {
+                "name": "Eintracht Frankfurt",
+                "price": 23
+              },
+              {
+                "name": "VfL Wolfsburg",
+                "price": 1.27
+              },
+              {
+                "name": "Draw",
+                "price": 4.5
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "key": "draftkings",
+        "title": "DraftKings",
+        "last_update": "2024-02-25T16:18:25Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2024-02-25T16:18:25Z",
+            "outcomes": [
+              {
+                "name": "Eintracht Frankfurt",
+                "price": 26
+              },
+              {
+                "name": "VfL Wolfsburg",
+                "price": 1.2
+              },
+              {
+                "name": "Draw",
+                "price": 5.25
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "key": "williamhill_us",
+        "title": "Caesars",
+        "last_update": "2024-02-25T16:18:25Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2024-02-25T16:18:25Z",
+            "outcomes": [
+              {
+                "name": "Eintracht Frankfurt",
+                "price": 23
+              },
+              {
+                "name": "VfL Wolfsburg",
+                "price": 1.24
+              },
+              {
+                "name": "Draw",
+                "price": 4.6
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "key": "unibet_us",
+        "title": "Unibet",
+        "last_update": "2024-02-25T16:18:26Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2024-02-25T16:18:26Z",
+            "outcomes": [
+              {
+                "name": "Eintracht Frankfurt",
+                "price": 23
+              },
+              {
+                "name": "VfL Wolfsburg",
+                "price": 1.22
+              },
+              {
+                "name": "Draw",
+                "price": 4.6
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "key": "mybookieag",
+        "title": "MyBookie.ag",
+        "last_update": "2024-02-25T16:18:25Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2024-02-25T16:18:25Z",
+            "outcomes": [
+              {
+                "name": "Eintracht Frankfurt",
+                "price": 15.5
+              },
+              {
+                "name": "VfL Wolfsburg",
+                "price": 1.29
+              },
+              {
+                "name": "Draw",
+                "price": 4.15
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "key": "wynnbet",
+        "title": "WynnBET",
+        "last_update": "2024-02-25T16:18:26Z",
+        "markets": [
+          {
+            "key": "h2h",
+            "last_update": "2024-02-25T16:18:26Z",
+            "outcomes": [
+              {
+                "name": "Eintracht Frankfurt",
+                "price": 35
+              },
+              {
+                "name": "VfL Wolfsburg",
+                "price": 1.2
+              },
+              {
+                "name": "Draw",
+                "price": 5
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+];
